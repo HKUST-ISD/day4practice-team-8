8353f4bc8ad2a28a7fb0b19ba0f3c4975bc4ccec
SAMCYK0921 <yzhangxp@connect.ust.hk>
2026-01-15 14:38:29 +0800

    P4_SAM
    哈哈哈 O-O

--- Ultrasonic_Skeleton/Ultrasonic_Skeleton.ino
+++ Ultrasonic_Skeleton/Ultrasonic_Skeleton.ino
@@ -1,12 +1,11 @@
-/*Replace ? in the code*/
 #include <Arduino.h>
 #include "NewPing.h"
 
 // Hook up HC-SR04 with Trig and Echo to ESP32 Pins
-#define TRIG_PIN ？                  
-#define ECHO_PIN ？  
+#define TRIG_PIN 5                  
+#define ECHO_PIN 4  
 // ESP 32 pin connected to ESP32
-#define LED ?
+#define LED 1
 
 // Maximum distance we want to ping for (in centimeters).
 #define MAX_DISTANCE 400
@@ -19,7 +18,8 @@ int value;
 
 void setup() {
   
-    Serial.begin(?); // Initialize the Serial Monitor and set a baud rate
+    Serial.begin(115200); // Initialize the Serial Monitor and set a baud rate
+    pinMode(LED, OUTPUT);
 }
 
 void loop() {
@@ -29,23 +29,27 @@ void loop() {
 
     // Print the distance to the Serial Monitor
     Serial.print("Distance: ");
-    Serial.print(?);
+    Serial.print(distance);
     Serial.println(" cm");
 
-    delay(?); // Delay 0.5s to avoid reading the distance too fast 
+    delay(500); // Delay 0.5s to avoid reading the distance too fast 
     
     // Distance > 10cm, LED is OFF
     // Distance< 10 cm, the closer the target, the brighter the LED 
     // set the duty cycle value in 8-bit (0-255) to use analogWrite() to generate PWM signal to set the LED brightness
-    if (?)
+    if (distance >= 10)
     {
-    value=?;
+        value = 0;
+    } else if (distance < 10 && distance >= 5)
+    {
+        value = (10 - distance) * 25; // Map distance 5-10cm to PWM 125-255
     } else
-    {value=?;}
+    {
+        value = 255;
+    }
     // print the duty cycle value 
     Serial.print("duty cycle value: ");
-    Serial.println(?); 
+    Serial.println(value); 
     // generate PWM signal to set the LED brightness
-    analogWrite(?,?);
-
+     analogWrite(LED, value);
 }
